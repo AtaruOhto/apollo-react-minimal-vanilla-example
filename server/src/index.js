c1c4a0f12f0be9671c0bb646031c280ca46695ba
Ataru Ohto <ataruohto@gmail.com>
2019-02-24 21:55:12 +0900

    fix wrong cors option provided in server

--- server/src/index.js
+++ server/src/index.js
@@ -1,29 +1,27 @@
 const { ApolloServer } = require("apollo-server");
-const typeDefs = require('./schema');
-const resolvers = require('./resolver')
-const User = require('./db/models/User');
-const { getDB } = require('./db')
+const typeDefs = require("./schema");
+const resolvers = require("./resolver");
+const User = require("./db/models/User");
+const { getDB } = require("./db");
 
 const boot = async () => {
   const store = await getDB();
 
   const server = new ApolloServer({
-    cors: { 
-      cors: {
-        origin: '*',
-        credentials: true,
-      }
+    cors: {
+      origin: "*",
+      credentials: true
     },
     typeDefs,
     resolvers,
     dataSources: () => ({
       User: new User({ store })
-    }),
+    })
   });
-  
+
   server.listen().then(({ url }) => {
     console.log(`🚀 Server ready at ${url}`);
   });
-}
+};
 
 boot();
