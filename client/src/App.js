7612e8f8f26487c6a6a8fc7c18d24180666013eb
Ataru Ohto <ataruohto@gmail.com>
2019-03-09 15:15:13 +0900

    chore: replaced update to refetchQueries

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import { ApolloProvider } from "react-apollo";
-import { ApolloProvider as ApolloHooksProvider, useMutation, useQuery } from "react-apollo-hooks";
+import {
+  ApolloProvider as ApolloHooksProvider,
+  useMutation,
+  useQuery
+} from "react-apollo-hooks";
 import { appClient } from "./graphql/client";
 import { GET_USERS, CREATE_USER } from "./graphql/tags/getUser";
 
@@ -9,51 +13,50 @@ const UserList = () => {
 
   if (loading) {
     return <div>Loading...</div>;
-  };
+  }
 
   if (error) {
     return `Error! ${error.message}`;
-  };
+  }
 
   return (
     <ul>
-      {
-        data.users.map(user => (<li key={user.id}>{user.name}</li>))
-      }
+      {data.users.map(user => (
+        <li key={user.id}>{user.name}</li>
+      ))}
     </ul>
   );
 };
 
 const UserInput = () => {
   const [state, setState] = useState("");
-  
+
   const createUser = useMutation(CREATE_USER, {
-    update: (proxy, { data: { createUser } }) => {
-      proxy.writeQuery({ query: GET_USERS,  data: {
-        users: createUser,
-      }, });
-    },
-    variables: { name: state },
+    refetchQueries: [
+      {
+        query: GET_USERS
+      }
+    ],
+    variables: { name: state }
   });
 
-  const onChange = (e) => {
+  const onChange = e => {
     setState(e.currentTarget.value);
-  }
-  
+  };
+
   return (
     <div>
       <input type="text" value={state} onChange={onChange} />
       <button onClick={createUser}>Add</button>
     </div>
-  )
-
-}
+  );
+};
 
 export const App = () => (
   <ApolloProvider client={appClient}>
     <ApolloHooksProvider client={appClient}>
       <UserInput />
-      <UserList/>
+      <UserList />
     </ApolloHooksProvider>
   </ApolloProvider>
-)
+);
